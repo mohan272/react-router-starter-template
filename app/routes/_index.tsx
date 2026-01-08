638e8eb50a02ab53f37bc001b263ccb55237efae
mohankumar-v-s <mohankumarvs.27@gmail.com>
2026-01-08 22:29:18 +0530

    fix package issues

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,6 @@
 import type { Route } from "./+types/_index";
-// import { Welcome } from "../welcome/welcome";
-import { Await, Link, useLoaderData } from "react-router";
-import { Suspense } from "react";
-import { UsersList, UsersSkeleton } from "~/components/Users";
+import { Link } from "react-router";
+
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -11,30 +9,14 @@ export function meta({ }: Route.MetaArgs) {
   ];
 }
 
-async function fetchUsers() {
-  await new Promise((r) => setTimeout(r, 2000));
 
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  if (!res.ok) throw new Error("API failed");
-  return res.json();
-}
-
-export async function loader() {
-  return ({
-    users: fetchUsers(),
-  });
-}
 /* ---------------- Page ---------------- */
 export default function HomePage() {
-  const { users } = useLoaderData();
 
   return (
     <section>
       <h2>Users</h2>
       <Link to="/userlist" className="underline">User List</Link>
-
     </section>
   );
 }
